688ba08fd4144fa037408be3d41e3bfc3fd11101
IGORSOKIL <rogi3839@gmail.com>
2023-07-28 14:17:44 +0300

    28.07.2023 Доповнено

--- c.bat
+++ c.bat
@@ -1,3 +1,3 @@
 rem cd D:\MRZV_HOME\Xtended\dnp3\CODE_opendnp3-mrzs\opendnp3-mrzs
- cd D:\MRZV_HOME\Xtended\dnp3\CODE_opendnp3-mrzs\opendnp3-mrzs\cpp\tests\TestOutstation_3
+ cd D:\MRZV_HOME\Xtended\dnp3\CODE_opendnp3-mrzs\opendnp3-mrzs\cpp\tests\TestLinkFrame_7
 start cc.bat
---
+++ cpp/tests/unit/utils/FormatUserData.cpp
@@ -0,0 +1,79 @@
+
+#include <QtWidgets>
+#include "header.h"
+#include "CRC.h"
+#include "LinkFrame.h"
+#include "LinkHeader.h"
+
+#include "WSeq.h"
+
+QString FormatUserData(
+  boolean aIsMaster, boolean aIsConfirmed, int aDest, int aSrc, /*const std::string& data,*/uint8_t* data, uint16_t size_data, boolean aFcb);/// = false)
+
+////std::string FormatUserData(
+////    bool aIsMaster, bool aIsConfirmed, int aDest, int aSrc, const std::string& data, bool aFcb = false)
+QString FormatUserData(
+  boolean aIsMaster, boolean aIsConfirmed, int aDest, int aSrc, /*const std::string& data,*/uint8_t* data, uint16_t size_data, boolean aFcb)/// = false)
+{
+  QString str_response = "";
+
+  RSeq_for_Uint16_t user_data;
+  RSeq_for_Uint16_t_in_RSeq_for_Uint16_tOver2(&user_data, data, size_data);
+////    HexSequence hs(data);
+////    REQUIRE(hs.Size() < 250);
+
+  uint8_t buffer[292];
+//    void WSeq_for_Uint16_t_in_WSeq_for_Uint16_tOver2(WSeq_for_Uint16_t *pWSeq_for_Uint16_t, uint8_t* buffer, uint16_t length);
+////    wseq_t wrapper(buffer, 292);
+  WSeq_for_Uint16_t wrapper;
+  WSeq_for_Uint16_t_in_WSeq_for_Uint16_tOver2(&wrapper, buffer, 292);
+
+  RSeq_for_Uint16_t rseq;
+
+  if (aIsConfirmed)
+  {
+//    RSeq_for_Uint16_t FormatConfirmedUserData_in_LinkFrame_static(WSeq_for_Uint16_t* buffer,
+//                                                   boolean aIsMaster,
+//                                                   boolean aFcb,
+//                                                   uint16_t aDest,
+//                                                   uint16_t aSrc,
+//                                                   RSeq_for_Uint16_t user_data);//,
+////        return HexConversions::to_hex(
+////            LinkFrame::FormatConfirmedUserData(wrapper, aIsMaster, aFcb, aDest, aSrc, hs, nullptr));
+    rseq =
+      FormatConfirmedUserData_in_LinkFrame_static(&wrapper,
+          aIsMaster,
+          aFcb,
+          aDest,
+          aSrc,
+          user_data);//,
+  }
+
+  else
+  {
+//    RSeq_for_Uint16_t FormatUnconfirmedUserData_in_LinkFrame_static(WSeq_for_Uint16_t* buffer,
+//                                                     boolean aIsMaster,
+//                                                     uint16_t aDest,
+//                                                     uint16_t aSrc,
+//                                                     RSeq_for_Uint16_t user_data);//,
+////    return HexConversions::to_hex(LinkFrame::FormatUnconfirmedUserData(wrapper, aIsMaster, aDest, aSrc, hs, nullptr));
+    rseq =
+      FormatUnconfirmedUserData_in_LinkFrame_static(&wrapper,
+          aIsMaster,
+          aDest,
+          aSrc,
+          user_data);//,
+  }//else
+
+//    uint16_t length_in_HasLength_for_Uint16_t(HasLength_for_Uint16_t *pHasLength);
+  uint16_t size = length_in_HasLength_for_Uint16_t(&(rseq.hHasLength));
+
+  for(int i=0; i<size; i++)
+  {
+    QString insertStr = ((QString("0%1").arg(QString::number(rseq.buffer_[i], 16))).right(2));
+    str_response.append(insertStr.toUpper());
+    str_response.append(" ");
+  }//for
+
+  return str_response;
+}
